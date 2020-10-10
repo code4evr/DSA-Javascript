a2511ca93c52b289b88e3bc43a843b83c94f022f
Bidit Upadhyay <bidit.upadhyay7@gmail.com>
2020-10-10 12:15:11 +0530

    singly linked list with add, remove and print functions

---
+++ singly-linked-list/LinkedList.js
@@ -0,0 +1,50 @@
+const Nodes = require('../Nodes');
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  addToHead(data) {
+    const newHead = new Nodes(data);
+    const currentHead = this.head;
+    this.head = newHead;
+    if (currentHead) {
+      this.head.setNextNode(currentHead);
+    }
+  }
+
+  addToTail(data) {
+    let tail = this.head;
+    if (!tail) {
+      this.head = new Nodes(data);
+    } else {
+      while (tail.getNextNode() !== null) {
+        tail = tail.getNextNode();
+      }
+      tail.setNextNode(new Nodes(data));
+    }
+  }
+
+  removeHead() {
+    const removedHead = this.head;
+    if (!removedHead) {
+      return;
+    }
+    this.head = removedHead.getNextNode();
+    return removedHead.data;
+  }
+
+  printList() {
+    let currentNode = this.head;
+    let output = '<head> ';
+    while (currentNode !== null) {
+      output += currentNode.data + ' ';
+      currentNode = currentNode.getNextNode();
+    }
+    output += '<tail>';
+    console.log(output);
+  }
+}
+
+module.exports = LinkedList;
