d645d50c07af6a38751b66a57d9433332ecfca52
Bidit Upadhyay <bidit.upadhyay7@gmail.com>
2020-11-18 23:09:56 +0530

    index.js file modified to work for both singly and doubly linked list

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 const LinkedList = require('./singly-linked-list/LinkedList');
+const DLinkedList = require('./doubly-linked-list/DoublyLinkedList');
 
 const colleges = new LinkedList();
+const family = new DLinkedList();
+console.log(family);
 
-const collegeNames = [
-  'IIT Madras',
-  'IIT Pune',
-  'IIT Kolkata',
-  'IIT Solan',
-  'IIT Mandi',
+// const collegeNames = [
+//   'IIT Madras',
+//   'IIT Pune',
+//   'IIT Kolkata',
+//   'IIT Solan',
+//   'IIT Mandi',
+// ];
+
+// collegeNames.map(college => {
+//   colleges.addToTail(college);
+// });
+
+// collegeNames.map(college => {
+//   console.log('the colleges are ', colleges);
+//   colleges.removeHead();
+// });
+
+// colleges.printList();
+
+const familyMembers = [
+  'Asha Upadhyay',
+  'Babu Boy',
+  'Binit Upadhyay',
+  'Saket Upadhyay',
 ];
 
-collegeNames.map(college => {
-  colleges.addToHead(college);
-});
-
-colleges.printList();
+familyMembers.map(fam => family.addToHead(fam));
+family.middleOfList();
+family.printList();
+family.removeByData('Bidit Upadhyay');
+console.log(
+  'after removing Bidit Upadhyay from the list, the new list is'
+);
+family.printList();
